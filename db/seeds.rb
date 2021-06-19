322bf5e7baa1c70ac6dcbd6e6816dcbc188bc489
Simon C <simoncorreaocampo@gmail.com>
2021-06-18 22:28:33 -0500

    Full seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
-# (1..10).each do |i|
-#   full_name = Faker::Movies::HarryPotter.character.split(" ")
-#   first_name = full_name[0]
-#   last_name = full_name[1]
-#   username = Faker::Movies::HarryPotter.spell
-#   email = "#{username}@sutraspa.com"
-#   User.create username: username, first_name: first_name, last_name: last_name, email: email, phone: Faker::PhoneNumber.cell_phone_in_e164, bio: Faker::Lorem.paragraph(sentence_count: 2), bg_color: Faker::Color.hex_color, remote_photo_url: Faker::Avatar.image
-# end
+(1..10).each do |i|
+  full_name = Faker::Movies::HarryPotter.character.split(" ")
+  first_name = full_name[0]
+  last_name = full_name[1]
+  username = Faker::Movies::HarryPotter.spell
+  email = "#{username}@sutraspa.com"
+  User.create username: username, first_name: first_name, last_name: last_name, email: email, phone: Faker::PhoneNumber.cell_phone_in_e164, bio: Faker::Lorem.paragraph(sentence_count: 2), bg_color: Faker::Color.hex_color, remote_photo_url: Faker::Avatar.image
+end
 
 puts "===> 1"
 
