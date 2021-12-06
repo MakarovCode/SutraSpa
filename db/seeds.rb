50ee79febd28afff598909ea6aacb2d1bef7042d
Simon C <simoncorreaocampo@gmail.com>
2021-12-05 21:02:02 -0500

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'dussanj@gmail.com', password: '12346578', password_confirmation: '12346578')
